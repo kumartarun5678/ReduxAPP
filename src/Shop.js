6b3e323c3576d28717ea8505a4552176ac61b56a
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2023-07-30 18:21:41 +0530

    ReduxAPP

---
+++ src/Shop.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
+import { useSelector } from "react-redux";
+
+function Shop() {
+    const dispatch = useDispatch();
+    const {withdrawMoney, depositMoney} =bindActionCreators(actionCreators,dispatch)
+    const balance = useSelector(state => state.amount)
+  return (
+    <div>
+      <h1>Buy a MacBook</h1>
+      {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+      Add to cart
+      <button className="btn btn-primary mx-2"onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button> */}
+      <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+      Update Money({balance})
+      <button className="btn btn-primary mx-2"onClick={()=>{depositMoney(100)}}>+</button>
+    </div>
+  );
+}
+
+export default Shop;
---
+++ src/state/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import amountReducer from "./amountReducer";
+const reducers = combineReducers({
+    amount:amountReducer
+})
+export default reducers;
